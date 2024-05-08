e1d556f0dd302c9c49ec8998bb95dc4bec4762d6
gabcmenezes <anguscamargo@gmail.com>
2024-05-08 18:56:49 -0300

    Add files via upload

---
+++ components/NomeUsuario.js
@@ -0,0 +1,10 @@
+function NomeUsuario(){
+    return(
+        <div>
+            <h1>Gabriel</h1>
+        </div>
+        
+    )
+}
+
+export default NomeUsuario
---
+++ components/SayMyName.js
@@ -0,0 +1,10 @@
+function SayMyName (props){
+    return(
+        <div>
+            <h1>
+                Seja bem-vindo, {props.nome}! Como você está?
+            </h1>
+        </div>
+    )
+}
+export default SayMyName
